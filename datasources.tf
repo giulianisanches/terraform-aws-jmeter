e2d870dee9191e412bf7e89cbe14e9b22bf57e31
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-04-05 17:40:12 -0300

    feat: query for a default ami

---
+++ datasources.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "amzn_linux_x64_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}

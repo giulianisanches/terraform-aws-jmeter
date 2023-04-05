331158ec728b9c54a6852c97020bafd55ada21c8
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-04-05 17:24:32 -0300

    fix: change instance name

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.jmeter_instance.public_ip
 }
 output "private_ip" {
   description = "The private IP address assigned to the instance."
-  value       = aws_instance.this.private_ip
+  value       = aws_instance.jmeter_instance.private_ip
 }
 
 output "instance_state" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = aws_instance.this.instance_state
+  value       = aws_instance.jmeter_instance.instance_state
 }
 
 output "arn" {
   description = "The ARN of the instance"
-  value       = aws_instance.this.arn
+  value       = aws_instance.jmeter_instance.arn
 }
 
 output "jmeter_version" {

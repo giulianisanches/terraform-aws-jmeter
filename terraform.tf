893a1ab16b6a4acfd50404ab6856b877a80f57eb
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-04-05 17:24:44 -0300

    feat: bump aws and terraform versions

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  required_version = "~> 1.4.0"
+}
